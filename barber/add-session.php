b3e42ff6c75e59bcc3a3ccfd097cbc1a9f8cf50e
kenzuue <129492936+kenzuue@users.noreply.github.com>
2023-05-05 17:53:20 +0800

    Add files via upload
    Signed-off-by: kenzuue <129492936+kenzuue@users.noreply.github.com>

---
+++ barber/add-session.php
@@ -0,0 +1,30 @@
+<?php
+
+    session_start();
+
+    if(isset($_SESSION["user"])){
+        if(($_SESSION["user"])=="" or $_SESSION['usertype']!='a'){
+            header("location: ../login.php");
+        }
+
+    }else{
+        header("location: ../login.php");
+    }
+    
+    
+    if($_POST){
+        //import database
+        include("../connection.php");
+        $title=$_POST["title"];
+        $barid=$_POST["barid"];
+        $nop=$_POST["nop"];
+        $date=$_POST["date"];
+        $time=$_POST["time"];
+        $sql="insert into schedule (barid,title,scheduledate,scheduletime,nop) values ($barid,'$title','$date','$time',$nop);";
+        $result= $database->query($sql);
+        header("location: schedule.php?action=session-added&title=$title");
+        
+    }
+
+
+?>
